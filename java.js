3305d0c85c0a92896ab1a1418843ec0b0b8ffe50
chippmusic@gmail.com <chippmusic@gmail.com>
2022-11-27 23:43:04 +0700

    first commit

---
+++ java.js
@@ -0,0 +1,18 @@
+// Bài Tập 1
+function theky() {
+    let ketQua2 = 2022; // Tạo 1 biến ketqua 2022
+    if(ketQua2 != null) { // Điều kiện
+      console.log("Năm " + ketQua2 + " Thế Kỷ " + Math.ceil(ketQua2 / 100)); // Công Thức math.celi làm tròn đến số nguyên lớn nhất tiếp theo
+    }
+}
+theky() // in ra kết quả
+
+// Bài tập 2
+function chuoi(){
+    let ten = ["Xin", "Chào", "Mình", "Tên", "Là"];
+    let ten2 = [" BTIC"]
+    console.log(ten.join(" ") + ten2); 
+}
+chuoi();
+
+// Bài Tập 3
